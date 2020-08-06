a62b7b0c012fcd5b8838ff89e9fc038a33ac75b7
Alexander Schreiner <schrein2@hm.edu>
2020-08-06 23:57:47 +0200

    bugfix

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,12 +1,12 @@
  import React, { Component } from 'react'
- import States from "./States.js"
+const States = ["new", "active", "closed", "blocked"]
 
 
  export default class Task extends Component {
     
     constructor(){
         super()
-        this.status = {completed: false, state = "new"};
+        this.status = {completed: false, state: "new"};
         this.setState = this.setState.bind(this)
     }
 
@@ -38,7 +38,7 @@
      render() {
          this.status["name"] = this.props.item.name
          this.status["description"] = this.props.item.description
-         this.status["acceptanceCriteria"] = this.props.item.acceptanceCriteria
+         this.status["acceptenceCriteria"] = this.props.item.acceptenceCriteria
          this.status["id"] = this.props.item.id
          this.status["priority"] = this.props.priority
          return (
@@ -48,9 +48,7 @@
                  <p>{this.props.item.description}</p>
                  <p>Acceptance criteria:</p>
                  <ul>
-                 {this.props.item.acceptanceCriteria.map(i => {
-                     <li>i</li>
-                 })}
+                 {this.props.item.acceptenceCriteria.map(i => <li>{i}</li>)}
                  </ul>
                 <p>priority: {this.props.item.priority}</p>
                 <hr/>
